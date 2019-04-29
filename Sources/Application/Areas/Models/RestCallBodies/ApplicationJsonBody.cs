0aae716bd594c1c3bae68a2ffa870093c6bf896e
Matthias Mueller <matthiasm@live.de>
2019-04-29 20:35:24 +0200

    WIP

--- Sources/Application/Areas/Models/RestCallBodies/ApplicationJsonBody.cs
+++ Sources/Application/Areas/Models/RestCallBodies/ApplicationJsonBody.cs
@@ -6,7 +6,6 @@ namespace Mmu.Mlh.RestExtensions.Areas.Models.RestCallBodies
     public class ApplicationJsonBody : RestCallBody
     {
         public const string MediaTypeAppJson = "application/json";
-
         public override string MediaType { get; } = MediaTypeAppJson;
 
         public ApplicationJsonBody(object payload) : base(payload)
