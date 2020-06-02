bcb1d211598706c541c66814e6cb46bc5ea49616
Matthias Müller <matthias.mueller@trivadis.com>
2020-06-02 14:24:57 +0200

    WIP

--- Sources/IntegrationTests/TestingAreas/Areas/RestProxies/Services/RestProxyIntegrationTests.cs
+++ Sources/IntegrationTests/TestingAreas/Areas/RestProxies/Services/RestProxyIntegrationTests.cs
@@ -1,112 +1,112 @@
-﻿////using System.Collections.Generic;
-////using System.Linq;
-////using System.Threading.Tasks;
-////using Mmu.Mlh.RestExtensions.Areas.RestProxies;
-////using Mmu.Mlh.RestExtensions.IntegrationTests.TestingInfrastructure.Context;
-////using Mmu.Mlh.RestExtensions.IntegrationTests.TestingInfrastructure.Models;
-////using Newtonsoft.Json;
-////using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mmu.Mlh.RestExtensions.Areas.RestProxies;
+using Mmu.Mlh.RestExtensions.IntegrationTests.TestingInfrastructure.Context;
+using Mmu.Mlh.RestExtensions.IntegrationTests.TestingInfrastructure.Models;
+using Newtonsoft.Json;
+using NUnit.Framework;
 
-////namespace Mmu.Mlh.RestExtensions.IntegrationTests.TestingAreas.Areas.RestProxies.Services
-////{
-////    [TestFixture]
-////    public class RestProxyIntegrationTests
-////    {
-////        [Test]
-////        public async Task PerformingCall_WithCorrectUrl_FetchesData()
-////        {
-////            // Arrange
-////            var context = RestTestContextBuilder.Create();
-////            var sut = context.ServiceLocator.GetService<IRestProxy>();
-////            var restCall = DataGenerator.CreateGetOneTodoRestCall();
+namespace Mmu.Mlh.RestExtensions.IntegrationTests.TestingAreas.Areas.RestProxies.Services
+{
+    [TestFixture]
+    public class RestProxyIntegrationTests
+    {
+        [Test]
+        public async Task PerformingCall_WithCorrectUrl_FetchesData()
+        {
+            // Arrange
+            var context = RestTestContextBuilder.Create();
+            var sut = context.ServiceLocator.GetService<IRestProxy>();
+            var restCall = DataGenerator.CreateGetOneTodoRestCall();
 
-////            // Act
-////            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
-////            var content = actualResponse.Content;
+            // Act
+            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
+            var content = actualResponse.Content;
 
-////            // Assert
-////            Assert.IsNotNull(content);
-////            Assert.AreEqual(1, content.Id);
-////            Assert.AreEqual(1, content.UserId);
-////            Assert.AreEqual("delectus aut autem", content.Title);
-////            Assert.AreEqual(false, content.Completed);
-////        }
+            // Assert
+            Assert.IsNotNull(content);
+            Assert.AreEqual(1, content.Id);
+            Assert.AreEqual(1, content.UserId);
+            Assert.AreEqual("delectus aut autem", content.Title);
+            Assert.AreEqual(false, content.Completed);
+        }
 
-////        [Test]
-////        public async Task PerformingCall_WithNotExistingUrl_ReturnsFailure()
-////        {
-////            // Arrange
-////            var context = RestTestContextBuilder.Create();
-////            var sut = context.ServiceLocator.GetService<IRestProxy>();
-////            var restCall = DataGenerator.CreateNotExistingUrlRestCall();
+        [Test]
+        public async Task PerformingCall_WithNotExistingUrl_ReturnsFailure()
+        {
+            // Arrange
+            var context = RestTestContextBuilder.Create();
+            var sut = context.ServiceLocator.GetService<IRestProxy>();
+            var restCall = DataGenerator.CreateNotExistingUrlRestCall();
 
-////            // Act
-////            var actualResponse = await sut.PerformCallAsync(restCall);
+            // Act
+            var actualResponse = await sut.PerformCallAsync(restCall);
 
-////            // Assert
-////            Assert.IsFalse(actualResponse.WasSuccess);
-////        }
+            // Assert
+            Assert.IsFalse(actualResponse.WasSuccess);
+        }
 
-////        [Test]
-////        public async Task PerformingCall_WithQueryParameters_FetchesData_OfPostIdOne()
-////        {
-////            // Arrange
-////            var context = RestTestContextBuilder.Create();
-////            var sut = context.ServiceLocator.GetService<IRestProxy>();
-////            var restCall = DataGenerator.CreateCommentsByPostIdRestCall();
+        [Test]
+        public async Task PerformingCall_WithQueryParameters_FetchesData_OfPostIdOne()
+        {
+            // Arrange
+            var context = RestTestContextBuilder.Create();
+            var sut = context.ServiceLocator.GetService<IRestProxy>();
+            var restCall = DataGenerator.CreateCommentsByPostIdRestCall();
 
-////            // Act
-////            var actualResponse = await sut.PerformCallAsync<List<Post>>(restCall);
-////            var content = actualResponse.Content;
+            // Act
+            var actualResponse = await sut.PerformCallAsync<List<Post>>(restCall);
+            var content = actualResponse.Content;
 
-////            // Assert
-////            Assert.AreEqual(5, content.Count);
-////            Assert.IsTrue(content.All(f => f.PostId == 1));
-////        }
+            // Assert
+            Assert.AreEqual(5, content.Count);
+            Assert.IsTrue(content.All(f => f.PostId == 1));
+        }
 
-////        [Test]
-////        public async Task PerformingPost_WithApplicationJsonFromJsonString_SendsAsJson()
-////        {
-////            // Arrange
-////            var context = RestTestContextBuilder.Create();
-////            var sut = context.ServiceLocator.GetService<IRestProxy>();
+        [Test]
+        public async Task PerformingPost_WithApplicationJsonFromJsonString_SendsAsJson()
+        {
+            // Arrange
+            var context = RestTestContextBuilder.Create();
+            var sut = context.ServiceLocator.GetService<IRestProxy>();
 
-////            var todo = new Todo { Completed = true, Title = "Hello Test", UserId = 123 };
-////            var jsonString = JsonConvert.SerializeObject(todo);
-////            var restCall = DataGenerator.CreatePostOneTodoRestCall(jsonString);
+            var todo = new Todo { Completed = true, Title = "Hello Test", UserId = 123 };
+            var jsonString = JsonConvert.SerializeObject(todo);
+            var restCall = DataGenerator.CreatePostOneTodoRestCall(jsonString);
 
-////            // Act
-////            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
-////            var content = actualResponse.Content;
+            // Act
+            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
+            var content = actualResponse.Content;
 
-////            // Assert
-////            Assert.IsNotNull(content);
-////            Assert.IsTrue(content.Id > 0);
-////            Assert.AreEqual(todo.Completed, content.Completed);
-////            Assert.AreEqual(todo.Title, content.Title);
-////            Assert.AreEqual(todo.UserId, content.UserId);
-////        }
+            // Assert
+            Assert.IsNotNull(content);
+            Assert.IsTrue(content.Id > 0);
+            Assert.AreEqual(todo.Completed, content.Completed);
+            Assert.AreEqual(todo.Title, content.Title);
+            Assert.AreEqual(todo.UserId, content.UserId);
+        }
 
-////        [Test]
-////        public async Task PerformingPost_WithApplicationJsonFromObject_ConvertsAndSendsAsJson()
-////        {
-////            // Arrange
-////            var context = RestTestContextBuilder.Create();
-////            var sut = context.ServiceLocator.GetService<IRestProxy>();
+        [Test]
+        public async Task PerformingPost_WithApplicationJsonFromObject_ConvertsAndSendsAsJson()
+        {
+            // Arrange
+            var context = RestTestContextBuilder.Create();
+            var sut = context.ServiceLocator.GetService<IRestProxy>();
 
-////            var todo = new Todo { Completed = true, Title = "Hello Test", UserId = 123 };
-////            var restCall = DataGenerator.CreatePostOneTodoRestCall(todo);
+            var todo = new Todo { Completed = true, Title = "Hello Test", UserId = 123 };
+            var restCall = DataGenerator.CreatePostOneTodoRestCall(todo);
 
-////            // Act
-////            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
-////            var content = actualResponse.Content;
+            // Act
+            var actualResponse = await sut.PerformCallAsync<Todo>(restCall);
+            var content = actualResponse.Content;
 
-////            // Assert
-////            Assert.IsNotNull(actualResponse);
-////            Assert.IsTrue(content.Id > 0);
-////            Assert.AreEqual(todo.Completed, content.Completed);
-////            Assert.AreEqual(todo.Title, content.Title);
-////            Assert.AreEqual(todo.UserId, content.UserId);
-////        }
-////    }
-////}
+            // Assert
+            Assert.IsNotNull(actualResponse);
+            Assert.IsTrue(content.Id > 0);
+            Assert.AreEqual(todo.Completed, content.Completed);
+            Assert.AreEqual(todo.Title, content.Title);
+            Assert.AreEqual(todo.UserId, content.UserId);
+        }
+    }
+}
